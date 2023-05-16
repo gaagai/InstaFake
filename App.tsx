48982ef09298af16bbed12a5657e364adc72864d
Gal Agai <gal@nbv.co.il>
2023-05-16 11:47:03 +0300

    React_Native-Instafake

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+      <Text>Hello World :)</Text>
+    </View>
+  );
+};
+
+export default App;
