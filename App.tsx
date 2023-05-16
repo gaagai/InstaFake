1a3116832770f6d9c60123b68f3bc8c0e05620c7
Gal Agai <gal@nbv.co.il>
2023-05-16 13:27:44 +0300

    vector icons

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import {Text, View} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   return (
     <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-      <Text>Hello World :)</Text>
+      <Text>Hello World )</Text>
+      <AntDesign name="stepforward" size={25} />
     </View>
   );
 };
